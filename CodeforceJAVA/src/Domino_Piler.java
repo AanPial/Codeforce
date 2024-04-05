c258a04c054aaa36d7484ef1deda3874af1a15fc
AanPial <165553604+AanPial@users.noreply.github.com>
2024-04-05 18:55:49 +0600

    Add files via upload

---
+++ CodeforceJAVA/src/Domino_Piler.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class Domino_Piler {
+    public static void main(String [] args){
+        Scanner p=new Scanner(System.in);
+        int m,n;
+        m=p.nextInt();
+        n=p.nextInt();
+
+        int x=(m*n)/2;
+
+        System.out.println(x);
+    }
+}
