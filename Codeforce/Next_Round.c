b2dbf6c8504c2a4de68af894794f70ed367a5536
AanPial <165553604+AanPial@users.noreply.github.com>
2024-04-05 17:26:14 +0600

    Add files via upload

---
+++ Codeforce/Next_Round.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main()
+{
+    int n,k;
+    scanf("%d%d",&n,&k);
+    int arr[n];
+    int i,count=0;
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+    for ( i = 0; i < n; i++)
+    {
+        if (arr[i]>arr[k-1] || arr[i]==arr[k-1] && arr[i]!=0)
+        {
+            count++;
+        }
+        
+    }
+    printf("%d",count);
+    
+}
