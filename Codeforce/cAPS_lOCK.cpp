5646bd314a6c87bb4bafd8a893d80c9632172070
AanPial <165553604+AanPial@users.noreply.github.com>
2024-09-03 01:01:23 +0600

    Update cAPS_lOCK.cpp

--- Codeforce/cAPS_lOCK.cpp
+++ Codeforce/cAPS_lOCK.cpp
@@ -2,41 +2,45 @@
 #include <cctype>
 using namespace std;
 
-int main() {
-    char str[101]; // Increased size to handle edge cases
+int main()
+{
+    char str[101];
     cin >> str;
 
     int x = 1, y = 0, z = 1;
 
-    // Check if all characters are uppercase
-    for (int i = 0; str[i] != '\0'; i++) {
-        if (islower(str[i])) {
+    for (int i = 0; str[i] != '\0'; i++)
+    {
+        if (islower(str[i]))
+        {
             x = 0;
             break;
         }
     }
-
-    // Check if the first character is lowercase
-    if (islower(str[0])) {
+    if (islower(str[0]))
+    {
         y = 1;
     }
-
-    // Check if all characters except the first one are uppercase
-    for (int i = 1; str[i] != '\0'; i++) {
-        if (islower(str[i])) {
+    for (int i = 1; str[i] != '\0'; i++)
+    {
+        if (islower(str[i]))
+        {
             z = 0;
             break;
         }
     }
-
-    // Convert based on the conditions
-    if (x == 1) { // All characters are uppercase
-        for (int i = 0; str[i] != '\0'; i++) {
+    if (x == 1)
+    {
+        for (int i = 0; str[i] != '\0'; i++)
+        {
             str[i] = tolower(str[i]);
         }
-    } else if (y == 1 && z == 1) { // First is lowercase, rest are uppercase
+    }
+    else if (y == 1 && z == 1)
+    {
         str[0] = toupper(str[0]);
-        for (int i = 1; str[i] != '\0'; i++) {
+        for (int i = 1; str[i] != '\0'; i++)
+        {
             str[i] = tolower(str[i]);
         }
     }
