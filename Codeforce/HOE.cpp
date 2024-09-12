398627194fe1a33ac9e85fd283fc924f40c04746
AanPial <165553604+AanPial@users.noreply.github.com>
2024-09-12 19:23:27 +0600

    Add files via upload

---
+++ Codeforce/HOE.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cin>>n;
+    int arr[n];
+    for(int i=0;i<n;i++)
+    {
+        cin>>arr[i];
+    }
+    int max=arr[0];
+    for(int i=0;i<n;i++)
+    {
+        if(arr[i]>max)
+        {
+            max=arr[i];
+        }
+    }
+    int sum=0;
+    for(int i=0;i<n;i++)
+    {
+        sum=sum+(max-arr[i]);
+    }
+    cout<<sum<<endl;
+
+    return 0;
+}
