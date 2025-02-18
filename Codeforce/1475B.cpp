01680b0e885a70d0b9899a0f25a92beb4d009e3e
AanPial <165553604+AanPial@users.noreply.github.com>
2025-02-18 16:35:32 +0600

    Add files via upload

---
+++ Codeforce/1475B.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int t;
+    cin >> t;
+    for (int i = 0; i < t; i++)
+    {
+        int n;
+        cin >> n;
+        if (n < 2020)
+        {
+            cout << "NO" << endl;
+        }
+        else if (n % 2020 == 0)
+        {
+            cout << "YES" << endl;
+        }
+        else if (n % 2021 == 0)
+        {
+            cout << "YES" << endl;
+        }
+        else
+        {
+            int p = n / 2020;
+            int q = n % 2020;
+            if (q <= p)
+            {
+                cout << "YES" << endl;
+            }
+            else
+            {
+                cout << "NO" << endl;
+            }
+        }
+    }
+}
